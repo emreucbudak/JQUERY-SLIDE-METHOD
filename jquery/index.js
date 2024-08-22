b564f1c65c6ec8070b4b25e411c3b4cece0813cd
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-22 12:39:00 +0300

    Add files via upload

---
+++ jquery/index.js
@@ -0,0 +1,6 @@
+// JQUERY METHODLARI
+// DETAYLI BILGI ICIN https://api.jquery.com/ 
+// ÖRNEK METHODLAR
+// TEXT METHODU
+// $("#id").text() belirtilen idye sahip elemanın sahip olduğu texti gösterir.
+// BAZI DIGER METHODLAR .val .addClass
